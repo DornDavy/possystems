43a9916303583e71b99a794c952bf406a22735a0
Dorn Davy <dorn.davy93@gmail.com>
2016-10-10 23:54:24 -0500

    Delete

--- app/Exchange.php
+++ app/Exchange.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Exchange extends Model
 {
     //
+    protected $fillable = [
+        'id',
+        ''
+    ];
 }
--- app/Promotion.php
+++ app/Promotion.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Promotion extends Model
 {
     //
+    protected $fillable = [
+        'id',
+        'name',
+        'end',
+        'amount',
+        'discount'
+    ];
 }
